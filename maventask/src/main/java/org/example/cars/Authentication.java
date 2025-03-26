4ef76cd6b6b8fb8f81735cb653f3c3105d0e2f2d
porostIslandzki <318632@office.umcs.pl>
2025-03-26 07:14:35 -0700

    changes added

--- maventask/src/main/java/org/example/cars/Authentication.java
+++ maventask/src/main/java/org/example/cars/Authentication.java
@@ -1,4 +1,27 @@
 package org.example.cars;
 
 public class Authentication {
+    private final IUserRepository userRepository;
+
+    public Authentication(IUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public boolean isAuthenticated(String login, String password) {
+        User user = userRepository.getUser(login);
+        if (user == null) {
+            System.out.println("Użytkownik nie istnieje.");
+            return false;
+        }
+
+        String hashedInput = org.apache.commons.codec.digest.DigestUtils.sha256Hex(password);
+
+        if (user.getPassword().equals(hashedInput)) {
+            System.out.println("Uwierzytelnienie zakończone sukcesem.");
+            return true;
+        } else {
+            System.out.println("Nieprawidłowe hasło.");
+            return false;
+        }
+    }
 }
